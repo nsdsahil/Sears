0beb9f627f9f216bc54a93f1668a42558aad44fe
Sahil Nishad <sahilroyal91@gmail.com>
2024-02-12 18:51:35 +0530

    changed api key of mail sender

--- Server/controllers/user.js
+++ Server/controllers/user.js
@@ -50,7 +50,7 @@ UserRouter.post("/register", async (req, res) => {
 			await userTemp.save();
 			let apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 			let apiKey = apiInstance.authentications["apiKey"];
-			apiKey.apiKey ="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";
+			apiKey.apiKey ="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";
 
 			let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
 
@@ -77,8 +77,8 @@ UserRouter.post("/register", async (req, res) => {
 				},
 				function (error) {
 					console.error(error);
-					res.status(200).send({
-						message: error.message,
+					return res.status(200).send({
+						message: "failed to send otp",
 					})
 				}
 			);
